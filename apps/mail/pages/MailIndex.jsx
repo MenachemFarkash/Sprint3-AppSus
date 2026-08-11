52b41e51885477d260402f3dcd38a13e24204aaf
r-ferdinaro <r.ferdinaro@tricentis.com>
2026-08-12 00:25:49 +0300

    Create basic index component

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,5 +1,40 @@
+import { mailService } from "../services/mail.service.js";
+import { showErrorMsg } from "../../../services/event-bus.service.js";
+
+import { MailFolderList } from "../cmps/MailFolderList.jsx";
+import { MailFilter } from "../cmps/MailFilter.jsx";
+import { MailList } from "../cmps/MailList.jsx";
+
+const { useState, useEffect } = React;
 
 export function MailIndex() {
-    return <section className="container">Mail app</section>
-}
+  const [mails, setMails] = useState(null);
+  const [selectedMail, setSelectedMail] = useState(null);
 
+  const [selectedFolder, setSelectedFolder] = useState(null);
+  const [filterBy, setFilterBy] = useState({});
+
+  useEffect(() => {
+    loadMails();
+  }, [filterBy]);
+
+  function loadMails() {
+    mailService
+      .query(filterBy)
+      .then(setMails)
+      .catch((err) => {
+        showErrorMsg(`Error loading mails from ${selectedFolder}`);
+        console.log(err);
+      });
+  }
+
+  return (
+    <section className="mail-container">
+      <MailFolderList />
+      <div>
+        <MailFilter />
+        <MailList />
+      </div>
+    </section>
+  );
+}
