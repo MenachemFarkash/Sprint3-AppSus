ae78c732926907614c7312e8df11a5da439047b3
r-ferdinaro <r.ferdinaro@tricentis.com>
2026-08-13 13:34:42 +0300

    Add lint

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js'
+import globals from 'globals'
+
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.js', '**/*.jsx'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            parserOptions: {
+                ecmaFeatures: { jsx: true }
+            },
+            globals: {
+                ...globals.browser,
+                React: 'readonly',
+                ReactRouterDOM: 'readonly'
+            }
+        },
+        rules: {
+            quotes: ['error', 'single'],
+            semi: ['error', 'never']
+        }
+    }
+]
