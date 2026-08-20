d2a3a9e8939052f0c9572705e567093e1d9654b4
r-ferdinaro <r.ferdinaro@tricentis.com>
2026-08-21 01:07:55 +0300

    Generalize AppHeader via per-app APP_CONFIG
    AppHeader/FilterSection stay generic (no per-app imports) and look up
    folder-nav visibility + filter behavior from a single APP_CONFIG map,
    keyed by app. Also switches AppHeader to an explicit 5-column grid and
    sizes the header/logo consistently.
    
    FilterSection itself doesn't consume the new props yet (next PR).

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -2,12 +2,17 @@ import { eventBusService } from '../services/event-bus.service.js'
 import { UserAvatar } from './UserAvatar.jsx'
 import { AppMenu } from './AppMenu.jsx'
 import { FilterSection } from './FilterSection.jsx'
+import { APP_CONFIG } from '../apps/apps.config.js'
 import { LOGGED_USER_FULLNAME, LOGGED_USER_COLOR } from '../services/user.service.js'
 
 const { Link, useLocation } = ReactRouterDOM
 
 export function AppHeader() {
-    const currApp = useLocation().pathname.split('/')[1]
+    const { pathname } = useLocation()
+    const currApp = pathname.split('/')[1]
+    const routeKey = pathname.split('/').slice(0, 4).join('/')
+
+    const { hasFolderNav, txtToFilterBy, filterByToTxt, advancedFilterFields } = APP_CONFIG[currApp] || {}
 
     function toggleFolderNav() {
         eventBusService.emit('toggle-folder-nav')
@@ -15,18 +20,28 @@ export function AppHeader() {
 
     return (
         <header className="app-header">
-            <button
-                className="folder-nav-toggle round-btn"
-                onClick={toggleFolderNav}
-            >
-                <img src="../assets/icons/folder-menu.svg" alt="folder-menu"/>
-            </button>
+            {hasFolderNav &&
+                <button
+                    className="folder-nav-toggle round-btn"
+                    onClick={toggleFolderNav}
+                >
+                    <img src="../assets/icons/folder-menu.icon.svg" alt="folder-menu" />
+                </button>
+            }
 
             <Link className="app-logo" to="/">
                 <img src={`../assets/icons/${currApp}.logo.png`} alt={`${currApp} logo`} />
             </Link>
 
-            <FilterSection currApp={currApp} />
+            {hasFolderNav &&
+                <FilterSection
+                    currApp={currApp}
+                    routeKey={routeKey}
+                    txtToFilterBy={txtToFilterBy}
+                    filterByToTxt={filterByToTxt}
+                    advancedFilterFields={advancedFilterFields}
+                />
+            }
 
             <AppMenu />
 
