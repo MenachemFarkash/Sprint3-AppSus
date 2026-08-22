117666d9923e09cffe8fa64b7b6717b6e95bccc6
r-ferdinaro <r.ferdinaro@tricentis.com>
2026-08-22 20:14:40 +0300

    Wire compose into mail index, folders nav, and preview
    Compose opens from the folder nav button or by opening a draft;
    autosaves while typing and hard-deletes empty drafts. Drafts show a
    'Draft' label in the mail list instead of the sender name.

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -4,25 +4,27 @@ import {
   showErrorMsg,
   showSuccessMsg,
 } from '../../../services/event-bus.service.js'
-
 import { FoldersNav } from '../../../cmps/FoldersNav.jsx'
+import { MailCompose } from '../cmps/MailCompose.jsx'
 import { MailDetails } from '../cmps/MailDetails.jsx'
 import { MailList } from '../cmps/MailList.jsx'
 import { useUnreadCounts } from '../hooks/useUnreadCounts.js'
 
 const { useState, useEffect } = React
 const { useParams } = ReactRouter
-const { useSearchParams } = ReactRouterDOM
+const { useSearchParams, useNavigate } = ReactRouterDOM
 
 export function MailIndex() {
   const { id: mailId, type: folderType } = useParams()
   const [searchParams] = useSearchParams()
+  const navigate = useNavigate()
   const filterBy = utilService.searchParamsToFilterBy(searchParams)
 
   const [allMails, setAllMails] = useState(null)
   const unreadCounts = useUnreadCounts(allMails)
 
   const [mails, setMails] = useState(null)
+  const [isComposing, setIsComposing] = useState(false)
 
   useEffect(() => {
     utilService.setFavicon('assets/icons/mail.icon.png')
@@ -71,21 +73,21 @@ export function MailIndex() {
       })
   }
 
-  // Hard-delete if mail is already in trash (removedAt set), otherwise move to trash
+  // Hard-delete if mail is already in trash (removedAt set) or is a draft, otherwise move to trash
   function onDeleteMail(mailId) {
     const mail = mails.find((currMail) => currMail.id === mailId)
 
-    if (mail.removedAt) {
+    if (mail.removedAt || !mail.sentAt) {
+      setMails((prev) => prev.filter((currMail) => currMail.id !== mailId))
+      showSuccessMsg(`Mail ${mailId} removed`)
+
       return mailService
         .remove(mailId)
-        .then(() => {
-          setMails((prev) => prev.filter((currMail) => currMail.id !== mailId))
-          showSuccessMsg(`Mail ${mailId} removed`)
-        })
         .then(loadAllMails)
         .catch((err) => {
           console.log(err)
           showErrorMsg(`Could not delete mail ${mailId}`)
+          loadMails()
         })
     }
 
@@ -93,13 +95,41 @@ export function MailIndex() {
       .then(() => showSuccessMsg('Mail moved to trash'))
   }
 
+  function onCompose() {
+    setIsComposing(true)
+  }
+
+  function onCloseCompose() {
+    setIsComposing(false)
+    if (folderType === 'draft') {
+      if (mailId) navigate('/mail/folder/draft')
+      loadMails()
+    }
+  }
+
+  function onMailSave() {
+    loadMails()
+    loadAllMails()
+  }
+
+  const isDraftCompose = folderType === 'draft' && Boolean(mailId)
+
   return (
     <section className="mail-index">
-      <FoldersNav app="mail" folders={FOLDER_TYPES} unreadCounts={unreadCounts} />
+      <FoldersNav app="mail" folders={FOLDER_TYPES} unreadCounts={unreadCounts} onCompose={onCompose} />
 
-      {mailId
+      {mailId && !isDraftCompose
         ? <MailDetails onUpdateMail={onUpdateMail} onDeleteMail={onDeleteMail} />
         : <MailList mails={mails} onUpdateMail={onUpdateMail} onDeleteMail={onDeleteMail} />
+      }
+
+      {(isComposing || isDraftCompose) &&
+        <MailCompose
+          key={isDraftCompose ? mailId : 'new'}
+          mailId={isDraftCompose ? mailId : null}
+          onClose={onCloseCompose}
+          onMailSave={onMailSave}
+        />
       }
     </section>
   )
